bd75da5c04e3005acf8f9a555d27731e2bdfc8b8
lyc9456783 <lyc9456783@163.com>
2018-07-07 17:06:41 +0800

    修改公共部分

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -1,14 +1,15 @@
 @extends('admin.common.common')
+
+@extends('admin.common.left')
+
+
 @section('content')    
         <!-- 右侧主体开始 -->
         <div class="page-content">
           <div class="content">
             <!-- 右侧内容框架，更改从这里开始 -->
             <blockquote class="layui-elem-quote">
-                注意：x-admin 1.1每个页面都可以独立设置一个背景主题，如果每个都设置会比较消耗本地的存储，如果想全部恢复，请重置。
-            </blockquote>
-            <blockquote class="layui-elem-quote">
-                欢迎使用x-admin 后台模版！<span class="f-14">v1.0</span>官方交流群： 519492808
+                欢迎来到京城商城管理后台！<span class="f-14">v1.0</span>
             </blockquote>
             <fieldset class="layui-elem-field layui-field-title site-title">
               <legend><a name="default">信息统计</a></legend>
@@ -42,14 +43,6 @@
                         <td>0</td>
                     </tr>
                     <tr>
-                        <td>昨日</td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
                         <td>本周</td>
                         <td>2</td>
                         <td>0</td>
@@ -76,95 +69,35 @@
                 <tbody>
                     <tr>
                         <th width="30%">服务器计算机名</th>
-                        <td><span id="lbServerName">http://127.0.0.1/</span></td>
+                        <td><span id="lbServerName">{{gethostbyname('127.0.0.1')}}</span></td>
                     </tr>
                     <tr>
                         <td>服务器IP地址</td>
-                        <td>192.168.1.1</td>
+                        <td>{{$_SERVER['SERVER_ADDR']}}</td>
                     </tr>
                     <tr>
                         <td>服务器域名</td>
-                        <td>x.xuebingsi.com</td>
+                        <td>www.lzyc.com</td>
                     </tr>
                     <tr>
                         <td>服务器端口 </td>
                         <td>80</td>
                     </tr>
                     <tr>
-                        <td>服务器IIS版本 </td>
-                        <td>Microsoft-IIS/6.0</td>
-                    </tr>
-                    <tr>
                         <td>本文件所在文件夹 </td>
-                        <td>D:\WebSite\HanXiPuTai.com\XinYiCMS.Web\</td>
+                        <td>{{__DIR__}}</td>
                     </tr>
                     <tr>
                         <td>服务器操作系统 </td>
-                        <td>Microsoft Windows NT 5.2.3790 Service Pack 2</td>
+                        <td>{{PHP_OS}}</td>
                     </tr>
                     <tr>
                         <td>系统所在文件夹 </td>
-                        <td>C:\WINDOWS\system32</td>
-                    </tr>
-                    <tr>
-                        <td>服务器脚本超时时间 </td>
-                        <td>30000秒</td>
-                    </tr>
-                    <tr>
-                        <td>服务器的语言种类 </td>
-                        <td>Chinese (People's Republic of China)</td>
-                    </tr>
-                    <tr>
-                        <td>.NET Framework 版本 </td>
-                        <td>2.050727.3655</td>
+                        <td>{{$_SERVER['DOCUMENT_ROOT']}}</td>
                     </tr>
                     <tr>
                         <td>服务器当前时间 </td>
-                        <td>2017-01-01 12:06:23</td>
-                    </tr>
-                    <tr>
-                        <td>服务器IE版本 </td>
-                        <td>6.0000</td>
-                    </tr>
-                    <tr>
-                        <td>服务器上次启动到现在已运行 </td>
-                        <td>7210分钟</td>
-                    </tr>
-                    <tr>
-                        <td>逻辑驱动器 </td>
-                        <td>C:\D:\</td>
-                    </tr>
-                    <tr>
-                        <td>CPU 总数 </td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>CPU 类型 </td>
-                        <td>x86 Family 6 Model 42 Stepping 1, GenuineIntel</td>
-                    </tr>
-                    <tr>
-                        <td>虚拟内存 </td>
-                        <td>52480M</td>
-                    </tr>
-                    <tr>
-                        <td>当前程序占用内存 </td>
-                        <td>3.29M</td>
-                    </tr>
-                    <tr>
-                        <td>Asp.net所占内存 </td>
-                        <td>51.46M</td>
-                    </tr>
-                    <tr>
-                        <td>当前Session数量 </td>
-                        <td>8</td>
-                    </tr>
-                    <tr>
-                        <td>当前SessionID </td>
-                        <td>gznhpwmp34004345jz2q3l45</td>
-                    </tr>
-                    <tr>
-                        <td>当前系统用户名 </td>
-                        <td>NETWORK SERVICE</td>
+                        <td>{{date('Y:m:d H:i:s',time())}}</td>
                     </tr>
                 </tbody>
             </table>
