3dfd786414caf3a7102d21bbfc7ab58a7299765e
yeshangjun1998 <17531371704@163.com>
2018-07-09 21:55:07 +0800

    asd

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,9 +18,26 @@ Route::get('/', function () {
 //后台首页
 Route::get('/admin','admin\IndexController@index');
 
-//测试
 
 
+//分类路由列表
+Route::get('/admin/cates','admin\CatesController@index');
+//分类添加
+Route::get('/admin/cates/create','admin\CatesController@create');
+//分类执行添加
+Route::post('/admin/cates/store','admin\CatesController@store');
+//分类修改页面
+Route::get('/admin/cates/edit/{id}','admin\CatesController@edit');
+//执行修改
+Route::post('/admin/cates/update/{id}','admin\CatesController@update');
+//分类删除页面
+Route::get('/admin/cates/destroy/{id}','admin\CatesController@destroy');
+//分类回收站
+Route::get('/admin/cates/getdel','admin\CatesController@getdel');
+//分类还原
+Route::get('/admin/cates/reset/{id}','admin\CatesController@reset');
+//分类永久删除
+Route::get('/admin/cates/delete/{id}','admin\CatesController@delete');
 
 
 
@@ -64,22 +81,5 @@ Route::get('/admin/entrepot/create','admin\EntrepotController@create');
 
 
 
-//分类路由列表
-Route::get('/admin/cates','admin\CatesController@index');
-//分类添加
-Route::get('/admin/cates/create','admin\CatesController@create');
-//分类执行添加
-Route::post('/admin/cates/store','admin\CatesController@store');
-//分类修改页面
-Route::get('/admin/cates/edit/{id}','admin\CatesController@edit');
-//执行修改
-Route::post('/admin/cates/update/{id}','admin\CatesController@update');
-//分类删除页面
-Route::get('/admin/cates/destroy/{id}','admin\CatesController@destroy');
-//分类回收站
-Route::get('/admin/cates/getdel','admin\CatesController@getdel');
-//分类还原
-Route::get('/admin/cates/reset/{id}','admin\CatesController@reset');
-//分类永久删除
-Route::get('/admin/cates/delete/{id}','admin\CatesController@delete');
+
 
