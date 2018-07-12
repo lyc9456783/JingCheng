c3450df66744bacc416e9ec74bbad9a8a56c1c46
liyinchang <xiaolitongzhi1996@126.com>
2018-07-12 09:17:03 +0800

    11

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,24 +17,6 @@ Route::get('/',function(){
 //后台主页路由
 Route::get('/admin','admin\IndexController@index');
 
-//分类路由列表
-Route::get('/admin/cates','admin\CatesController@index');
-//分类添加
-Route::get('/admin/cates/create','admin\CatesController@create');
-//分类执行添加
-Route::post('/admin/cates/store','admin\CatesController@store');
-//分类修改页面
-Route::get('/admin/cates/edit/{id}','admin\CatesController@edit');
-//执行修改
-Route::post('/admin/cates/update/{id}','admin\CatesController@update');
-//分类删除页面
-Route::get('/admin/cates/destroy/{id}','admin\CatesController@destroy');
-//分类回收站
-Route::get('/admin/cates/getdel','admin\CatesController@getdel');
-//分类还原
-Route::get('/admin/cates/reset/{id}','admin\CatesController@reset');
-//分类永久删除
-Route::get('/admin/cates/delete/{id}','admin\CatesController@delete');
 
 
 
@@ -68,17 +50,6 @@ Route::get('/admin/cates/delete/{id}','admin\CatesController@delete');
 
 
 
-//后台评论
-Route::get('/admin/discuss','admin\DiscussController@index');
-Route::get('/admin/discuss/create','admin\DiscussController@create');
-Route::post('/admin/discuss/store','admin\DiscussController@store');
-Route::get('/admin/discuss/edit/{id}','admin\DiscussController@edit');
-Route::post('/admin/discuss/update/{id}','admin\DiscussController@update');
-Route::get('/admin/discuss/destroy/{id}','admin\DiscussController@destroy');
-Route::get('/admin/discuss/delete','admin\DiscussController@delete');
-//后台库存
-Route::get('/admin/entrepot','admin\EntrepotController@index');
-Route::get('/admin/entrepot/create','admin\EntrepotController@create');
 
 
 
@@ -132,38 +103,67 @@ Route::get('/admin/entrepot/create','admin\EntrepotController@create');
 
 
 
-//设置引入后台用户列表的模板
-Route::get('/admin/users/index','admin\UsersController@index');
 
-//设置引入后台用户添加的模板
-Route::get('/admin/users/create','admin\UsersController@create');
 
-//设置用户添加到数据库
-Route::post('/admin/users/store','admin\UsersController@store');
 
-//设置用户修改
-Route::get('/admin/users/edit/{id}','admin\UsersController@edit');
 
-//设置用户修改
-Route::get('/admin/users/pass/{id}','admin\UsersController@pass');
 
-//设置用户修改
-Route::post('/admin/users/update/{id}','admin\UsersController@update');
 
-//设置用户删除
-Route::get('/admin/users/del/{id}','admin\UsersController@del');
 
-//设置用户删除
-Route::get('/admin/users/destroy','admin\UsersController@destroy');
 
-//设置用户信息恢复还原
-Route::get('/admin/users/reset/{id}','admin\UsersController@reset');
 
-//设置永久删除数据
-Route::get('/admin/users/delete/{id}','admin\UsersController@delete');
 
-//设置用户修改密码
-Route::post('admin/users/passupdate/{id}','admin\UsersController@passupdate');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
